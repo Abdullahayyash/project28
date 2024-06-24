5e80821ed937911473fc9bca246e106a444242e5
KareemMasri142 <kareemmasri4@gmail.com>
2024-06-24 12:25:09 +0300

    Update adc.c

--- adc.c
+++ adc.c
@@ -2,20 +2,23 @@
 #define SBIT_ADON     0
 #define SBIT_CHS0     3
 #define SBIT_ADFM     7
+// Function to create a delay for the ADC
 void delay_adc(int cnt)
 {
     while(cnt--);
 }
+// Function to initialize the ADC module 
 void ADC_Init()
  {
    ADCON0=0x00;  
    ADCON1=(1<<SBIT_ADFM);  
 }
+// Function to read the ADC value from a specified channel
 int ADC_Read(int adcChannel)
  {  
     ADCON0 = (1<<SBIT_ADON) | (adcChannel<SBIT_CHS0);  
 
-    delay_adc(1000);                   
+    delay_adc(1000);    // Call delay function               
    
     GO=1;                           // the ADC conversion will start
     while(GO_DONE==1);              // Wait for it to complete
