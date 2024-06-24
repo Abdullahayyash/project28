8d9587a61ad44c2b415b19dc88565e504008f0df
KareemMasri142 <kareemmasri4@gmail.com>
2024-06-24 12:31:05 +0300

    Update eusart.h

--- eusart.h
+++ eusart.h
@@ -7,11 +7,11 @@
 #ifdef __cplusplus  
 #endif
 #define EUSART1_DataReady  (PIR1bits.RC1IF)
-void EUSART1_Initialize(void);
-uint8_t EUSART1_Read(void);
-void EUSART1_Write(uint8_t txData);
-void put_string(const unsigned char* str);
-void UART_RxString(char *string_ptr);
+void EUSART1_Initialize(void); // Function prototype to initialize the EUSART1 module
+uint8_t EUSART1_Read(void); // Function prototype to read a byte of data from EUSART1
+void EUSART1_Write(uint8_t txData); // Function prototype to write a byte of data to EUSART1
+void put_string(const unsigned char* str); // Function prototype to send a string via EUSART1
+void UART_RxString(char *string_ptr); // Function prototype to receive a string via EUSART1 
 #ifdef __cplusplus  
 #endif
 #endif 
